587d017e336fd33fb865f081ff11a213fa70dfe9
Jesús <jesus.garciap@iesnervion.es>
2024-03-03 13:22:40 +0100

    FuncionesRecursivas Hecho

---
+++ src/clases/FuncionesRecursivasTest.java
@@ -0,0 +1,63 @@
+package clases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class FuncionesRecursivasTest {
+
+	@ParameterizedTest
+	@MethodSource("probandoSumatorio")
+	void testSumatorio(int numero, int expected) {
+		int resultado = FuncionesRecursivas.sumatorio(numero);
+		assertEquals(resultado, expected);
+	}
+
+	private static Stream<Arguments> probandoSumatorio(){
+		return Stream.of(
+				Arguments.of(1,1),
+				Arguments.of(2,3),
+				Arguments.of(3,6)
+				);
+		
+	}
+
+	@ParameterizedTest
+	@MethodSource("probandoPotencia")
+	void testPotencia(double a, int n, double expected) {
+		double resultado = FuncionesRecursivas.potencia(a, n);
+		assertEquals(resultado, expected);
+	}
+	
+	private static Stream<Arguments> probandoPotencia(){
+		return Stream.of(
+				Arguments.of(0, 5, 0),
+				Arguments.of(4.6, 0, 1),
+				Arguments.of(2, 1, 2),
+				Arguments.of(3, 2, 9)
+				);
+		
+	}
+
+	@ParameterizedTest
+	@MethodSource("probandoFibonacci")
+	void testFibonacci(int numero, int expected) {
+		int resultado = FuncionesRecursivas.fibonacci(numero);
+		assertEquals(resultado, expected);
+	}
+	
+	private static Stream<Arguments> probandoFibonacci(){
+		return Stream.of(
+				Arguments.of(1,1),
+				Arguments.of(2,1),
+				Arguments.of(3,2),
+				Arguments.of(5,5)
+				);
+		
+	}
+
+}
